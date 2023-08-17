47b66b92e84eb21cd8e2e4a4cc2ca2a710a3348d
toxicmix <72023909+toxicmix@users.noreply.github.com>
2023-08-16 20:51:22 -0500

    added connection

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "employees"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+});
+
+module.exports = connection;
